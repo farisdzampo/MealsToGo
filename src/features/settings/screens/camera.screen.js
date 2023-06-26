aad819ef8d354347f057e0716ee0051b6b741891
farisdzampo <faredzampo23@gmail.com>
2023-06-26 20:45:26 +0200

    Camera capture button

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,14 +1,36 @@
 import React, { useState, useRef, useEffect, useContext } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import styled from "styled-components/native";
 import { Camera, CameraType } from "expo-camera";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ErrorText } from "../../account/components/account.styles";
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
+const cameraIcon = <Ionicons name="camera-outline" color="white" size={60} />;
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
+`;
+
+const CaptureButtonContainer = styled.View`
+  flex: 1;
+  background-color: "transparent";
+  flex-direction: "column";
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const CaptureButton = styled.TouchableOpacity`
+  align-self: center;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background-color: tomato;
+  margin-bottom: 30px;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const CameraScreen = ({ navigation }) => {
@@ -40,11 +62,13 @@ export const CameraScreen = ({ navigation }) => {
   }
 
   return (
-    <TouchableOpacity onPress={snap}>
-      <ProfileCamera
-        ref={(cam) => (cameraRef.current = cam)}
-        type={CameraType.front}
-      ></ProfileCamera>
-    </TouchableOpacity>
+    <ProfileCamera
+      ref={(cam) => (cameraRef.current = cam)}
+      type={CameraType.front}
+    >
+      <CaptureButtonContainer>
+        <CaptureButton onPress={snap}>{cameraIcon}</CaptureButton>
+      </CaptureButtonContainer>
+    </ProfileCamera>
   );
 };
